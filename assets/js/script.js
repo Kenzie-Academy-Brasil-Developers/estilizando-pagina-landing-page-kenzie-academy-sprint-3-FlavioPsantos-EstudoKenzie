9b9daadd03a65a8bac3342d8698c28c7cecad7db
Victor Varela <vitaotapioqueiro@gmail.com>
2021-07-06 11:09:00 -0300

    pausa 1 - finalizando menu hamburguer mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,17 @@
 
 /* PARTE VICTOR */
 
+const mostrarMenu = document.querySelector('#menu__open');
+const fechaMenu = document.querySelector('#menu__close');
+const modal = document.querySelector('#modal');
 
+mostrarMenu.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    mostrarMenu.classList.add('hidden');
+    fechaMenu.classList.remove('hidden');
+});
 
-
+console.log('oi')
 
 /* PARTE FLÁVIO */
 
