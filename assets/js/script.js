f71d7c9231c6779d4c81a07d543064d763a387a7
Victor Varela <vitaotapioqueiro@gmail.com>
2021-07-06 19:58:49 -0300

    mobile first, falta @medias

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,22 @@ const mostrarMenu = document.querySelector('#menu__open');
 const fechaMenu = document.querySelector('#menu__close');
 const modal = document.querySelector('#modal');
 
-mostrarMenu.addEventListener('click', () => {
+const openMenu = (event) => {
     modal.classList.remove('hidden');
     mostrarMenu.classList.add('hidden');
     fechaMenu.classList.remove('hidden');
-});
+}
 
-console.log('oi')
+const closeMenu = (event) => {
+    modal.classList.add('hidden');
+    mostrarMenu.classList.remove('hidden');
+    fechaMenu.classList.add('hidden');
+}
+
+
+mostrarMenu.addEventListener('click', openMenu);
+
+fechaMenu.addEventListener('click', closeMenu)
 
 /* PARTE FLÁVIO */
 
